c945157c22d1ba5d22f750f1366a95f08f80267e
BSD <remodelpc@gmail.com>
2023-10-04 15:02:23 +1000

    add skelet

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,34 @@
 #include <Arduino.h>
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+#include <NetBIOS.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <WebServer.h>
+
+#include <Ticker.h>
+#include "time.h"
+
+#include "espnow.h"
+#include "config.h"
+#include "wifi_conf.h"
+#include "times.h"
+#include "web_server.h"
+
+#include <Arduino_JSON.h>
+
+#if FILESYSTEM == FFat
+#include <FFat.h>
+#endif
+#if FILESYSTEM == SPIFFS
+#include <SPIFFS.h>
+#endif
+
+#include "SI7021.h"
+#include "ds18b.h"
 
 // put function declarations here:
 int myFunction(int, int);
